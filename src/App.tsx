cb1321845a1fe2ad68da3f542ab1555fcc1b007c
apaolelli <a.paolelli@ns12.it>
2026-03-11 18:14:17 +0100

    test deploy

--- src/App.tsx
+++ src/App.tsx
@@ -4,36 +4,6 @@ import Glitch from './appComponents/Glitch';
 
 function App() {
 
-  //const [appare, setAppare] = useState(false)
-
-  /*   useEffect(() => {
-      const enableFullscreen = () => {
-        const elem = document.documentElement; // Seleziona l'intera pagina
-  
-        // Funzione per gestire i vari browser
-        const requestFS =
-          elem.requestFullscreen
-  
-        if (requestFS) {
-          requestFS.call(elem)
-            .then(() => {
-              console.log("Fullscreen attivato con successo!");
-            })
-            .catch((err) => {
-              console.error(`Errore nel tentativo di attivare il fullscreen: ${err.message}`);
-            });
-        }
-  
-        // IMPORTANTE: Rimuoviamo il listener subito dopo il primo click
-        document.removeEventListener('click', enableFullscreen);
-      };
-  
-      // Aggiungiamo il listener globale
-      document.addEventListener('click', enableFullscreen);
-  
-      // Pulizia se il componente viene smontato
-      return () => document.removeEventListener('click', enableFullscreen);
-    }, []); */
 
   return (
 
@@ -49,7 +19,8 @@ function App() {
           position: 'absolute',
           display: 'flex',
           flexDirection: 'column',
-          justifyContent: 'flex-end'
+          justifyContent: 'flex-end',
+          colorScheme: "dark"
         }}>
           <div>
             <span>
